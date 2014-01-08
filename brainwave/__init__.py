f71d5ad2284b7b1f462ff02a0046c2deac355127
Snuggert <abe.wiersma@hotmail.nl>
2014-01-08 13:57:00 +0100

    StartUp: Bram je bent me toch een duiveltje and dots before local imports

--- brainwave/__init__.py
+++ brainwave/__init__.py
@@ -12,9 +12,13 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # Register blueprints
-from brainwave.controllers.user import user_controller
-from brainwave.views.admin import admin_blueprint
-
+from .brainwave.controllers.user import user_controller
+from .brainwave.views.admin import admin_blueprint
 
 app.register_blueprint(user_controller)
 app.register_blueprint(admin_blueprint)
+
+# Add methods and modules to jinja environment
+from .brainwave.utils import serialize_sqla
+app.jinja_env.globals.update(json=json)
+app.jinja_env.globals.update(serialize_sqla=serialize_sqla)
