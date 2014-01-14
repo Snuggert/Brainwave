48b03c96004233a125afd591a811b4892bf7a63f
Jaap Koetsier <mail@jkoetsier.nl>
2014-01-14 14:46:59 +0100

    Admin login

--- brainwave/forms/validation.py
+++ brainwave/forms/validation.py
@@ -6,17 +6,18 @@ import re
 class Validation():
     """ Class that contains validation functions """
 
-    def validate_username(form, field):
+    def validate_username(username):
         """ Validates username. Must start with a letter, follow by letters,
         numbers, - or _. Minimum length is 5 characters.
         """
-        return re.match(r'[A-Za-z]+[A-Za-z0-9]{4,}', field.data)
+        return re.match(r'[A-Za-z]+[A-Za-z0-9_-]{4,}', username)
 
-    def validate_password(form, field):
+    def validate_password(password):
         """ Validates password. Minimum length is 8 characters, can contain
         letters, numbers, @, #, $, %, ^ & + and =.
         """
-        return re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', field.data)
+        return re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', password)
 
-    def validate_email(form, field):
-        pass
+    def validate_email(email):
+        # Yet to be implemented
+        return False
