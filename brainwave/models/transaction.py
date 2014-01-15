27664eb0e5e91ef5ca8f663649daf0e1687ac684
Snuggert <abe.wiersma@hotmail.nl>
2014-01-15 17:02:32 +0100

    I did somewhat the same, added trans_in boolean, i realise it is a bad thing to work on the same stuff at the same moment, so ill stop here until later

--- brainwave/models/transaction.py
+++ brainwave/models/transaction.py
@@ -6,16 +6,17 @@ from brainwave.utils.base_model import BaseEntity
 class Transaction(db.Model, BaseEntity):
     """Transaction model."""
     __tablename__ = 'transaction'
-    
+
+    trans_in = db.Column(db.Boolean)
+    pay_type = db.Column(db.Enum('cash', 'pin', name='pay_type'))
+    status = db.Column(db.Enum('pending', 'paid', 'cancelled', name='status'))
     assoc_id = db.Column(db.Integer, db.ForeignKey('association.id'))
 
-    pay_type = db.Column(db.Enum('cash', 'pin', name='pay_type'))
+    pieces = db.relationship('TransactionPiece', backref='transaction')
 
-    status = db.Column(db.Enum('pending', 'paid', 'cancelled', name='status'))
-
-    transactions = db.relationship('TransactionPiece', backref='transaction')
-
-    def __init__(self, assoc_id=None, pay_type='cash', status='cancelled'):
-        self.assoc_id = assoc_id
+    def __init__(self, pay_type='cash', status='pending', association=None,
+                 trans_in=False):
         self.pay_type = pay_type
         self.status = status
+        self.association = association
+        self.trans_in = trans_in
