3df356de587419980f1522fbd35f16890de74f3a
steinvanzwoll <steinvanzwoll@gmail.com>
2014-01-15 09:02:28 +0100

    transaction done

--- brainwave/static/js/sale.js
+++ brainwave/static/js/sale.js
@@ -1,3 +1,5 @@
+var receipt = [];
+
 $(document).ready(function () {
     load_products();
     reshape_product();
@@ -22,11 +24,28 @@ function load_products () {
             console.log(data);
             for (i = 0; i < data.products.length; i++) {
                 $('#product-list').append(
-                    "<a href='#' class='product btn'>"+ 
+                    "<a href='#' class='product btn btn-default btn-primary' " +
+                    "onclick='add_product(" + data.products[i].id + ")'>"+ 
                     data.products[i].shortname + "</a>"
                 );
             }
         }
     });
+}
 
+function add_product (id) {
+    receipt.push({"product_id": id.toString(), "action":"sell"});
+}
+
+function complete_transaction (pay_type) {
+    $(document).ready(function(){
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            url: "../api/transaction/new",
+            data: '{"pay_type": "'+ pay_type +'", "items": '+ JSON.stringify(receipt) +'}',
+            dataType: "json"
+        });
+    });
+    receipt = [];
 }
