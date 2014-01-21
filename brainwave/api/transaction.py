9f4613914be1375b4d6d881692b8c974cf269a48
Mats ten Bohmer <mtenbohmer@gmail.com>
2014-01-21 14:52:24 +0100

    Small change in the transaction api

--- brainwave/api/transaction.py
+++ brainwave/api/transaction.py
@@ -5,6 +5,7 @@ from brainwave.controllers.transaction import TransactionController
 transaction_api = Blueprint('transaction_api', __name__,
                             url_prefix='/api/transaction')
 
+
 # Dictionary format example:
 # {
 #    "pay_type":"cash",
@@ -22,7 +23,7 @@ transaction_api = Blueprint('transaction_api', __name__,
 #
 # pay_type can be either cash or pin (credit should become option 3, later)
 # action can be sell, refund or gift
-@transaction_api.route('/new', methods=['POST'])
+@transaction_api.route('', methods=['POST'])
 def create():
     """Create a new transaction."""
     dict = request.json
