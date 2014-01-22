d4166d8dde324dbbfabe8e8cb5db5933b65a387d
steinvanzwoll <steinvanzwoll@gmail.com>
2014-01-22 16:23:08 +0100

    improved t9.

--- brainwave/static/js/t9.js
+++ brainwave/static/js/t9.js
@@ -1,13 +1,18 @@
-var names = ["Bas Klaplong", "Bram van den Akker", "Jaap Koetsier", "Mats ten Bohme",
+/*
+var names = ["Bas Klaplong", "Bram van den Akker", "Jaap Koetsier", "Mats ten Bohmer",
              "Stein van Zwoll", "Sander", "Juliën", "Pieter", "Gijs", "ÛØËÎ"];
+names.sort();
+*/
+var customers;
 var letters = ["clear", "space", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"];
 var letters_acc = ["clear", " ", "ABCÀÁÅÃÆÇàáâãäåæç", "DEFÈÉÊËèéêë", "GHIÌÍÎÏìíîï", 
                    "JKL", "MNOÑñÒÓÔÕÖØòóôõöøð", "PQRSß", "TUVÙÚÛÜùúûü", "WXYZýþÿÝÞ"];
 var squares = ['.number-key'];
 var pattern = "";
-
+var numbers = "";
 
 $(document).ready(function () {
+    load_customers();
     add_number_keys();
     reshape_squares();
     display_names();
@@ -15,21 +20,38 @@ $(document).ready(function () {
 
 function add_to_pattern (number) {
     if (letters[number]) {
-        pattern = pattern + "[" + letters_acc[number] + "]";  
+        pattern = pattern + "[" + letters_acc[number] + "]";
+        numbers = numbers + number;
     }
     if (letters[number] == "clear") {
         pattern = "";
+        numbers = "";
     }
-    console.log(pattern);
+    //console.log(pattern);
+    display_names();
+}
+
+function pattern_backspace () {
+    numbers = numbers.substr(0, numbers.length-1);
+    pattern = pattern.substr(0, pattern.lastIndexOf("["));
     display_names();
 }
 
 function display_names () {
     $("#names").html("");
+    $("#names").append(numbers + "<br />");
     var filter = new RegExp(pattern, "i");
-    for (var i = 0; i < names.length; i++) {
-        if (!filter || filter.test(names[i])) {
-            $("#names").append(names[i] + "<br />");
+    for (var i = 0; i < customers.length; i++) {
+        name = customers[i].name;
+        matches = filter.exec(name);
+        if (!filter || matches) {
+            var match = matches[0];
+            match_pos = name.indexOf(match);
+            $("#names").append("<a onclick='select_customer(\"" + i + "\")'>" + 
+                               name.substr(0,match_pos) + "<b>" +  
+                               name.substr(match_pos, match.length) +
+                               "</b>" + name.substr(match_pos + match.length, name.length) +
+                               "</a>" + "<br />");
         }
     }
 }
@@ -39,6 +61,13 @@ function reshape_squares () {
         var cw = $(squares[i]).width();
         $(squares[i]).css({'height': cw + 'px'});
     }
+}
+
+function select_customer (id) {
+    alert(customers[id].name + " selected.");
+    pattern = "";
+    numbers = "";
+    display_names();
 }
 
 function add_number_keys () {
@@ -51,4 +80,21 @@ function add_number_keys () {
                 i+ " " + letters[i] + "</a>"
         );
     } while (i != 0);
+    
+    $('#number-pad').append(
+        "<a href='#' class='number-key btn btn-default btn-primary col-md-4' " +
+            "onclick='pattern_backspace()'>backspace</a>"
+    );
+}
+
+function load_customers () {
+    var result = $.ajax({
+        type: "GET",
+        url: "/api/customer/all",
+        async: false,
+        success: function (data) {
+            console.log(data);
+            customers=data.customers;
+        }  
+    });
 }
