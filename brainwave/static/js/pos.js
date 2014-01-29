68b871093920622104789342bace090467bfe9c8
Mats ten Bohmer <mtenbohmer@gmail.com>
2014-01-29 20:54:15 +0100

    Bugfix on the category navigation in the POS system

--- brainwave/static/js/pos.js
+++ brainwave/static/js/pos.js
@@ -295,7 +295,10 @@ var ProductButtonView = Backbone.View.extend({
     },
     on_category_switch: function(data) {
         this.category_id = data.category_id;
-        this.render();
+        
+        this.$el.find('.col-sm-3').show();
+        if (this.category_id > 0)
+            this.$el.find('.col-sm-3').not('.product-cat-' + this.category_id).hide();
     }
 });
 
