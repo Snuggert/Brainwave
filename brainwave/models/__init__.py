d3571e4a301abe9c63ccb5b45508bf978df7e8f2
Snuggert <abe.wiersma@hotmail.nl>
2014-01-29 16:12:08 +0100

    forgot to add new model to init, my bad

--- brainwave/models/__init__.py
+++ brainwave/models/__init__.py
@@ -5,6 +5,7 @@ from .product_category import ProductCategory
 from .product import Product
 from .stock import Stock
 from .trans_in import TransIn
+from .trans_out import TransOut
 from .user import User
 from .transaction import Transaction
 from .transaction_piece import TransactionPiece
